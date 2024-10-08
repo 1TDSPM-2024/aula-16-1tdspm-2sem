29babbb106b119474a20e8fcbb8bed6fb004a383
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-10-08 08:46:55 -0300

    adicionando loading

---
+++ aula16/src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Spinner from "@/components/Spinner/Spinner"
+
+export default function Loading() {
+  return (
+    <div className="text-center">
+        <h1>Loading...</h1>
+        <Spinner />
+    </div>
+  )
+}

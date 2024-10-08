d82df403919ee10fbde7e2aef0ed2944a3eb0ff8
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-08 08:24:37 -0300

    Corrigindo regra tailwind, estilizando home e tabela e adicionando capa.

--- aula16/src/app/page.tsx
+++ aula16/src/app/page.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+import imgCapa from "@/img/black-friday-facebook-cover-banner-10.jpg";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="prose-base lg:prose-2xl">Página Principal</h1>
+    <div className="content-wrap">
+      {/* <Image src="/img/black-friday-facebook-cover-banner-10.jpg" alt="Fones de ouvido"/> */}
+      <Image src={imgCapa} alt="Fones de ouvido"/>
     </div>
   )
 }

8a9378cb09d216d0ef10f7af3225ab8c1755d4c9
irissuu <iriswinx2@gmail.com>
2024-10-08 09:42:51 -0300

    O endpoint do POST foi implementado.

--- aula16/src/app/api/base-produtos/route.tsx
+++ aula16/src/app/api/base-produtos/route.tsx
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from "fs";
+import { TipoProduto } from "@/types";
 
 export async function GET() {
 
@@ -8,4 +9,37 @@ export async function GET() {
     //PARSEAR O ARQUIVO
     const produtos = JSON.parse(file);
     return NextResponse.json(produtos);
+}
+
+export async function POST(request:Request) {
+
+    //Chamada para o arquivo base.json
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json" , "utf-8");
+
+    //PARSEAR O ARQUIVO QUE A NOSSA LISTA DE PRODUTOS
+    const produtos:TipoProduto[] = JSON.parse(file);
+
+    //Destructuring no request para obter os dados que foram enviados para gravação na lista.
+    const {id ,nome, qtd} = await request.json();
+
+    //Agora montamos um novo objeto com os dados desestruturados e tipamos:
+    const novoProduto:TipoProduto = {
+        id: id,
+        nome: nome,
+        qtd: qtd
+        };
+
+        //gerando um novo ID para o produto:
+        novoProduto.id = ( produtos[ produtos.length -1 ].id +1 )
+
+        //Agora vamos adicionar o novo produto na lista de produtos que recuperamos do arquivo:
+        produtos.push(novoProduto);
+
+        //Tranformando a lista em JSON/string para depois colocar de volta no arquivo:
+        const fileJson = JSON.stringify(produtos)
+
+        //Agora devolvemos a lista de produtos no arquivo JSON sobrepondo os dados antigo pelos novos:
+        await fs.writeFile(process.cwd() + "/src/data/base.json" , fileJson);
+
+        return NextResponse.json(novoProduto);
 }
--- aula16/src/types.ts
+++ aula16/src/types.ts
@@ -1,4 +1,5 @@
 export type TipoProduto = {
+    push(novoProduto: TipoProduto): unknown;
 
     id:number;
     nome:string;
